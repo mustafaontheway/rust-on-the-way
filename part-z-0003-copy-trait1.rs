1416d6adc36c46a404c6ef0b85fc4be36188f32d
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-08-26 13:05:08 +0300

    Create part-z-0003-copy-trait1.rs

---
+++ part-z-0003-copy-trait1.rs
@@ -0,0 +1,21 @@
+fn main() {
+
+    let her_age: u8 = 36;
+
+    let age_data = her_age;
+
+    println!("{age_data}");
+
+    println!("{her_age}");
+
+    let ready = true;
+
+    let _res = ready;
+
+    let name = "Mustafa";
+
+    let _my_name = name;
+}
+
+// 36
+// 36
