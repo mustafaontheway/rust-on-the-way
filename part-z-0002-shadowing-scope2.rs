fb6e5a216e71dd4002bd90b3e67133329789896c
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-08-26 12:55:53 +0300

    Create part-z-0002-shadowing-scope2.rs

---
+++ part-z-0002-shadowing-scope2.rs
@@ -0,0 +1,18 @@
+fn main() {
+
+    let my_data: u8 = 17;
+
+    println!("My data (print 1): {my_data}");
+
+    let my_data: f32 = 3.3456;
+
+    println!("My data (print 2): {my_data}");
+
+    let my_data = false;
+
+    println!("My data (print 3): {my_data}");
+}
+
+// My data (print 1): 17
+// My data (print 2): 3.3456
+// My data (print 3): false
