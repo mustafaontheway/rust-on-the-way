e75c84b1b399cb2a54cb2da5e986a964e41a6a72
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-08-27 09:24:21 +0300

    Create part-z-0006-positional-arguments1.rs

---
+++ part-z-0006-positional-arguments1.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+    let name1 = String::from("Mustafa");
+    let name2 = String::from("Aygün");
+    let name3 = String::from("Kutluk");
+
+    println!("{2} - {0} - {1}", name1, name2, name3)
+}
+
+// Kutluk - Mustafa - Aygün
