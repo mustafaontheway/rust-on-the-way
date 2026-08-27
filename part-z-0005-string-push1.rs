1a14cc46006d2ec4908c6a6eb1ceb0cfe50dc2cf
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-08-27 09:18:34 +0300

    Create part-z-0005-string-push1.rs

---
+++ part-z-0005-string-push1.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+    let mut name = String::new();
+
+    name = "Aybük".to_string();
+
+    name.push('e');
+
+    name.push_str(" Kartanesi");
+
+    println!("{name}");
+}
+
+// Aybüke Kartanesi
