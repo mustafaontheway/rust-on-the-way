85c97473c2ccab993f5086f602a5ea3f0e92de90
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-08-28 14:18:30 +0300

    Create part-z-0008-usize-isize1.rs

---
+++ part-z-0008-usize-isize1.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    let ages: [u8; 5] = [17, 21, 9, 99, 55];
+
+    let age_index_1: usize = 3;
+
+    let age_index_2 = 1; // auto usize!
+
+    println!("Age 1: {}", ages[age_index_1]);
+
+    println!("Age 2: {}", ages[age_index_2]);
+
+    let _neg_val: isize = -500;
+}
+
