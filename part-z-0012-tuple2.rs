31abeb6daf46376d4e2751a57c4b5291846c3590
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-08-30 11:20:27 +0300

    Create part-z-0012-tuple2.rs

---
+++ part-z-0012-tuple2.rs
@@ -0,0 +1,19 @@
+fn main() {
+
+    let mut info_emp_ayhan: (&'static str, String, u16) = ("Ayhan Bilir", "Finance Department".to_string(), 4300);
+
+    info_emp_ayhan.2 = 4700;
+
+    println!("Employee name: {}", info_emp_ayhan.0);
+    println!("Employee department: {}", info_emp_ayhan.1);
+    println!("Employee salary: ${}", info_emp_ayhan.2);
+
+    let (_name, department, _salary_usd) = &info_emp_ayhan;
+
+    println!("Employee department: {}", department);
+}
+
+// Employee name: Ayhan Bilir
+// Employee department: Finance Department
+// Employee salary: $4700
+// Employee department: Finance Department
