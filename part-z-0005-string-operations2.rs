111aa68677370dd8e7fe356299d4d308474acbf1
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-09-10 05:57:38 +0300

    Create part-z-0005-string-operations2.rs

---
+++ part-z-0005-string-operations2.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+    let name_1 = "Mustafa".to_string();
+
+    let name_2 = "Kültigin".to_string();
+
+    let last_name = "Büyükdereli".to_string();
+
+    // let full_name = name_1 + " " + &name_2 + " " + last_name; // Error: main.rs(9, 52): consider borrowing here: `&`
+
+    let full_name = name_1 + " " + &name_2 + " " + &last_name; // Error: main.rs(9, 52): consider borrowing here: `&`
+
+    println!("Full name: {full_name}")
+}
+
+// Full name: Mustafa Kültigin Büyükdereli
