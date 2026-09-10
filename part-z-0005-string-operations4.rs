afdf5321b60481891113328839c06afe78bc756e
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-09-10 06:09:52 +0300

    Create part-z-0005-string-operations4.rs

---
+++ part-z-0005-string-operations4.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+    let full_name = "    Büyükdereli Mustafa Kültigin ".to_string();
+
+    println!("{}", full_name.trim());
+
+    println!("{}", full_name.trim_start());
+
+    println!("{}", full_name.trim_end());
+}
