2992fc62466dab891512a276039d4f2008e3e23e
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-09-16 05:39:36 +0300

    Create part-z-0032-vector-creation1.rs

---
+++ part-z-0032-vector-creation1.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+    let _ages = vec![17u8, 77, 55, 97];
+
+    let _diffs: Vec<i16> = vec![-2400, 172, 88, -500];
+
+    let _years = Vec::from([81u8, 77, 14, 0]);
+
+    let mut _names: Vec<String> = Vec::new();
+}
