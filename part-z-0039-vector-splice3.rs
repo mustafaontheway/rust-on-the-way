df84a723d748bbf5f30132b8b943b1307ac230fe
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-09-16 06:21:57 +0300

    Create part-z-0039-vector-splice3.rs

---
+++ part-z-0039-vector-splice3.rs
@@ -0,0 +1,12 @@
+fn main() {
+
+    let mut ages: Vec<u8> = vec![10, 20, 30, 40];
+
+    let our_ages = [77u8, 99, 109, 89];
+
+    ages.splice(2..2, our_ages);
+
+    println!("Ages: {:?}", ages);
+}
+
+// Ages: [10, 20, 77, 99, 109, 89, 30, 40]
