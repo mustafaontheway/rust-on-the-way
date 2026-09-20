ed978dd2b84f69ec1a202e93e2a5b67f163597b3
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-09-20 21:09:36 +0300

    Update part-y-0100-generics-intro1.rs

--- part-y-0100-generics-intro1.rs
+++ part-y-0100-generics-intro1.rs
@@ -1,1 +1,16 @@
+use std::ops::Add;
 
+fn main() {
+
+    let _sum1 = sum_vals(3.123f32, 43.555);
+
+    let _sum2 = sum_vals(-21i16, 92);
+}
+
+fn sum_vals<T>(x: T, y: T) -> T 
+
+where 
+    T: Add<Output =  T>,
+{
+    x + y
+}
