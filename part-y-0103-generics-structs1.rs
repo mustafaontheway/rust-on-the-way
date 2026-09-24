df6c17b4cea10d358a8a403641a4b5ac99df97d9
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-09-24 22:44:38 +0300

    Create part-y-0103-generics-structs1.rs

---
+++ part-y-0103-generics-structs1.rs
@@ -0,0 +1,13 @@
+fn main() {
+
+    let _player1_c = Coordinates { x: 4u8, y: 11, z: 9};
+
+    let _player2_c: Coordinates<f32> = Coordinates { x: -3.21, y: 11.11, z: -41.21};
+}
+
+struct Coordinates<T> {
+
+    x: T,
+    y: T,
+    z: T
+}
